650ac80b0af5d2f6db031e77312e3ced19d13650
anthonynahas <anthony.na@hotmail.de>
2018-04-09 21:19:21 +0200

    feat(ngx-material-pages): added synchronization between the page content and outlook

--- src/module/components/ngx-material-pages/ngx-material-pages.component.ts
+++ src/module/components/ngx-material-pages/ngx-material-pages.component.ts
@@ -9,6 +9,7 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import {NgxMaterialPageLoaderComponent} from './ngx-material-page-loader/ngx-material-page-loader.component';
+import {StepperSelectionEvent} from '@angular/cdk/stepper';
 
 @Component({
   selector: 'ngx-material-pages',
@@ -21,7 +22,14 @@ export class NgxMaterialPagesComponent implements OnInit, AfterContentInit {
 
   @ContentChildren(NgxMaterialPageLoaderComponent) pages: QueryList<NgxMaterialPageLoaderComponent>;
 
+  // current page
+  index: number;
+
+  // the total number of pages to render
+  totalPages: number;
+
   constructor() {
+    this.index = 0;
   }
 
   ngOnInit() {
@@ -29,6 +37,30 @@ export class NgxMaterialPagesComponent implements OnInit, AfterContentInit {
 
   ngAfterContentInit() {
     console.log('pagesOutlook', this.pages);
+    this.totalPages = this.pages.length;
+  }
+
+  /**
+   * Go to the previous page if it exists
+   */
+  previous() {
+    console.log('index before', this.index);
+    this.index = this.index === 0 ? 0 : --this.index;
+    console.log('index after', this.index);
+  }
+
+  /**
+   * Go to the next page if it exists
+   */
+  next() {
+    console.log('index before', this.index);
+    this.index = this.index === this.totalPages - 1 ? this.totalPages - 1 : ++this.index;
+    console.log('index after', this.index);
+  }
+
+  onStepSelectionChange(event: StepperSelectionEvent) {
+    console.log('event = ', event);
+    this.index = event.selectedIndex;
   }
 
 }
