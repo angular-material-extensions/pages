a75058239bdfdffdf2f1176161c666b827eb7349
anthonynahas <anthony.na@hotmail.de>
2018-04-10 22:02:15 +0200

    feat(lib): added mode to sidenav and template navigation buttons + added pageChanged and done events

--- src/module/components/ngx-material-pages/ngx-material-pages.component.ts
+++ src/module/components/ngx-material-pages/ngx-material-pages.component.ts
@@ -2,23 +2,37 @@ import {
   AfterContentInit,
   ChangeDetectionStrategy,
   Component,
-  ContentChildren,
-  forwardRef,
-  OnInit,
-  QueryList,
+  ContentChildren, EventEmitter,
+  forwardRef, HostListener,
+  OnInit, Output,
+  QueryList, ViewChild,
   ViewEncapsulation
 } from '@angular/core';
 import {NgxMaterialPageLoaderComponent} from './ngx-material-page-loader/ngx-material-page-loader.component';
 import {StepperSelectionEvent} from '@angular/cdk/stepper';
+import {MatSidenav} from '@angular/material';
 
 @Component({
   selector: 'ngx-material-pages',
   templateUrl: './ngx-material-pages.component.html',
-  styleUrls: ['./ngx-material-pages.component.scss'],
+  styleUrls: ['./ngx-material-pages.component.scss']
 })
 export class NgxMaterialPagesComponent implements OnInit, AfterContentInit {
 
-  @ContentChildren(NgxMaterialPageLoaderComponent) pages: QueryList<NgxMaterialPageLoaderComponent>;
+  @ViewChild('sidenav')
+  sidenav: MatSidenav;
+
+  @ContentChildren(NgxMaterialPageLoaderComponent)
+  pages: QueryList<NgxMaterialPageLoaderComponent>;
+
+  @Output()
+  done: EventEmitter<void> = new EventEmitter<void>();
+
+  @Output()
+  pageChanged: EventEmitter<number> = new EventEmitter<number>();
+
+  // the mode of the sidenav
+  navMode = 'side';
 
   // current page
   index: number;
@@ -30,35 +44,57 @@ export class NgxMaterialPagesComponent implements OnInit, AfterContentInit {
     this.index = 0;
   }
 
+  private _initSidenavMode(): void {
+    if (window.innerWidth < 768) {
+      this.navMode = 'over';
+    }
+  }
+
   ngOnInit() {
+    this._initSidenavMode();
   }
 
   ngAfterContentInit() {
-    console.log('pagesOutlook', this.pages);
     this.totalPages = this.pages.length;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < 768) {
+      this.navMode = 'over';
+      this.sidenav.close();
+    }
+    if (event.target.innerWidth > 768) {
+      this.navMode = 'side';
+      this.sidenav.open();
+    }
   }
 
   /**
    * Go to the previous page if it exists
    */
   previous() {
-    console.log('index before', this.index);
     this.index = this.index === 0 ? 0 : --this.index;
-    console.log('index after', this.index);
+    this.pageChanged.emit(this.index);
   }
 
   /**
    * Go to the next page if it exists
    */
   next() {
-    console.log('index before', this.index);
     this.index = this.index === this.totalPages - 1 ? this.totalPages - 1 : ++this.index;
-    console.log('index after', this.index);
+    this.pageChanged.emit(this.index);
   }
 
+
+  /**
+   * Update the value of the index when a step has been selected.
+   * Emit this value for pagechanged's subscribers.
+   *
+   */
   onStepSelectionChange(event: StepperSelectionEvent) {
-    console.log('event = ', event);
     this.index = event.selectedIndex;
+    this.pageChanged.emit(this.index);
   }
 
 }
