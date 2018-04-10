27aea5a98dfdc682456ca968f264567fc0e90c48
anthonynahas <anthony.na@hotmail.de>
2018-04-10 22:02:50 +0200

    demo(app): updated demo app to support the last features of ngx-material-pages

--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Title }     from '@angular/platform-browser';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService:Title) { }
+  constructor(private titleService: Title) {
+  }
 
   ngOnInit() {
     this.titleService.setTitle('Home | ngx-material-pages');
