4b90675f5001dc967dbfe6492ce7af7cf737ed9f
anthonynahas <anthony.na@hotmail.de>
2018-04-11 23:18:23 +0200

    demo(app): added the required module to the gettingstartedModule (material + highlight + disqus)

--- demo/src/app/getting-started/getting-started.module.ts
+++ demo/src/app/getting-started/getting-started.module.ts
@@ -1,13 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GettingStartedComponent } from './getting-started.component';
-import { GettingStartedRoutingModule } from './getting-started-routing.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GettingStartedComponent} from './getting-started.component';
+import {GettingStartedRoutingModule} from './getting-started-routing.module';
+import {MatButtonModule, MatCardModule, MatDividerModule, MatIconModule, MatSnackBarModule, MatTooltipModule} from '@angular/material';
+import {HighlightModule} from 'ngx-highlightjs';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ClipboardModule} from 'ngx-clipboard';
+import {NgxMaterialPagesModule} from 'ngx-material-pages';
+import {DisqusModule} from 'ngx-disqus';
 
 @NgModule({
   imports: [
     CommonModule,
-    GettingStartedRoutingModule
+    GettingStartedRoutingModule,
+    NgxMaterialPagesModule.forRoot(),
+    DisqusModule.forRoot('ngx-material-pages'),
+    HighlightModule.forRoot(),
+    ClipboardModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSnackBarModule
   ],
   declarations: [GettingStartedComponent],
 })
-export class GettingStartedModule { }
+export class GettingStartedModule {
+}
