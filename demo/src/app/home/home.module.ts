c944b6f33f56f7ce9d3419f3ebd2b7897d9fe9fb
anthonynahas <anthony.na@hotmail.de>
2018-04-16 19:15:07 +0200

    demo(app): added some cards and tabs - minor

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import {NgxMaterialPagesModule} from 'ngx-material-pages';
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from './home.component';
 import {DisqusModule} from 'ngx-disqus';
+import {MatCardModule, MatTabsModule} from '@angular/material';
 
 @NgModule({
   imports: [
@@ -12,6 +13,8 @@ import {DisqusModule} from 'ngx-disqus';
     NgxMaterialPagesModule.forRoot(),
     DisqusModule.forRoot('ngx-material-pages'),
     HomeRoutingModule,
+    MatCardModule,
+    MatTabsModule
   ],
   declarations: [HomeComponent],
 })
