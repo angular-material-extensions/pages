4706a477934db2290ce5afc43785bf9e7214d3ef
anthonynahas <anthony.na@hotmail.de>
2018-04-22 20:19:14 +0200

    demo(app): updated rxjs dependency

--- demo/src/app/shared/shared.module.ts
+++ demo/src/app/shared/shared.module.ts
@@ -1,15 +1,16 @@
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {ContentWrapperComponent} from './content-wrapper/content-wrapper.component';
 
 @NgModule({
-    imports: [RouterModule, NgbCollapseModule.forRoot() ],
-    exports: [HeaderComponent, FooterComponent, ContentWrapperComponent],
-    declarations: [HeaderComponent, FooterComponent, ContentWrapperComponent],
-    providers: [],
+  imports: [RouterModule, NgbCollapseModule.forRoot()],
+  exports: [HeaderComponent, FooterComponent, ContentWrapperComponent],
+  declarations: [HeaderComponent, FooterComponent, ContentWrapperComponent],
+  providers: [],
 })
-export class AppSharedModule { }
+export class AppSharedModule {
+}
